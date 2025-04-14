5c8e5d988dee0248a6d6c43d2cbe39bd745e2836
Abdullah <abdullahtopal0@gmail.com>
2025-04-14 15:22:54 +0300

    commit

---
+++ src/main/java/com/example/questapp/services/CommentService.java
@@ -0,0 +1,57 @@
+package com.example.questapp.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.example.questapp.entities.Comment;
+import com.example.questapp.entities.Post;
+import com.example.questapp.entities.User;
+import com.example.questapp.repos.CommentRepository;
+import com.example.questapp.requests.CommentCreateRequest;
+
+@Service
+public class CommentService {
+	
+	private CommentRepository commentRepository;
+	private UserService userService;
+	private PostService postService;
+
+	public CommentService(CommentRepository commentRepository, UserService userService, PostService postService) {
+		this.commentRepository = commentRepository;
+	}
+
+	public List<Comment> getAllCommentsWithParam(Optional<Long> userId, Optional<Long> postId) {
+		if(userId.isPresent() && postId.isPresent()) {
+			return commentRepository.findByUserIdAndPostId(userId.get(), postId.get());
+		} else if(userId.isPresent()) {
+			return commentRepository.findByUserId(userId.get());
+		} else if(postId.isPresent()) {
+			return commentRepository.findByPostId(postId.get());
+		} else {
+			return commentRepository.findAll();
+		}
+	}
+
+	public Comment getOneCommentById(Long commentId) {
+		return commentRepository.findById(commentId).orElse(null);
+	}
+
+	public Comment createOneComment(CommentCreateRequest request) {
+		User user = userService.getOneUser(request.getUserId());
+		Post post = postService.getOnePostById(request.getPostId());
+		if(user !=null && post != null) {
+			Comment commentToSave = new Comment();
+			commentToSave.setId(request.getId());
+			commentToSave.setPost(post);
+			commentToSave.setUser(user);
+			commentToSave.setText(request.getText());
+			return commentRepository.save(commentToSave);
+		} else
+			return null;
+	}
+	
+	
+	
+}
